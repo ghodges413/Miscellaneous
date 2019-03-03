b71b778268e20b1b5700d6c4f5629b7510878744
ghodges413 <38189889+ghodges413@users.noreply.github.com>
2019-03-03 15:42:10 -0600

    Refactored random to be purely static

--- RayTracingWeekend/book1/code/Random.cpp
+++ RayTracingWeekend/book1/code/Random.cpp
@@ -6,6 +6,9 @@
 #include "Random.h"
 #include "Vector.h"
 
+std::mt19937 Random::m_generator( 0 );
+std::uniform_real_distribution< float > Random::m_distribution( 0.0f, 1.0f );
+
 /*
 ====================================================
 Random::RandomInUnitSphere
--- RayTracingWeekend/book1/code/Random.h
+++ RayTracingWeekend/book1/code/Random.h
@@ -14,12 +14,10 @@ Random
 */
 class Random {
 public:
-	Random() : m_generator( 0 ), m_distribution( 0.0f, 1.0f ) {}
-	
-	float Get() { return m_distribution( m_generator ); }	// Gets a uniform distribution in the range [ 0, 1 )
-	Vec3d RandomInUnitSphere();
+	static float Get() { return m_distribution( m_generator ); }	// Gets a uniform distribution in the range [ 0, 1 )
+	static Vec3d RandomInUnitSphere();
 
 private:
-	std::mt19937 m_generator;
-	std::uniform_real_distribution< float > m_distribution;
+	static std::mt19937 m_generator;
+	static std::uniform_real_distribution< float > m_distribution;
 };
