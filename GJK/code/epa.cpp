3eef623c821130d4a754e9a67124b01f90300ae6
ghodges413 <38189889+ghodges413@users.noreply.github.com>
2019-03-31 19:38:26 -0500

    moved plane into its own file

--- GJK/code/epa.cpp
+++ GJK/code/epa.cpp
@@ -10,71 +10,7 @@
 #include <vector>
 #include "epa.h"
 #include "gjk.h"
-
-/*
-========================================================================================================
-
-plane_t
-
-========================================================================================================
-*/
-struct plane_t {
-	Vec3d m_pts[ 3 ];
-	Vec3d m_normal;
-
-	plane_t() {}
-	plane_t( Vec3d a, Vec3d b, Vec3d c ) {
-		m_pts[ 0 ] = a;
-		m_pts[ 1 ] = b;
-		m_pts[ 2 ] = c;
-
-		m_normal = Normal();
-	}
-
-	void FlipNormal() {
-		std::swap( m_pts[ 0 ], m_pts[ 1 ] );
-		m_normal = Normal();
-	}
-
-	Vec3d Normal() const {
-		const Vec3d & a = m_pts[ 0 ];
-		const Vec3d & b = m_pts[ 1 ];
-		const Vec3d & c = m_pts[ 2 ];
-
-		const Vec3d ab = b - a;
-		const Vec3d ac = c - a;
-
-		Vec3d normal = ab.Cross( ac );
-		normal.Normalize();
-		return normal;
-	}	
-
-	float SignedDistanceToPlane( const Vec3d & point ) const {
-		const Vec3d dirToPoint = point - m_pts[ 0 ];
-		float distance = Normal().DotProduct( dirToPoint );
-		return distance;
-	}
-
-	float UnsignedDistanceToPlane( const Vec3d & point ) const {
-		return fabsf( SignedDistanceToPlane( point ) );
-	}
-
-	bool IsCoPlanar( const Vec3d & point, const float epsilon = 0.00001f ) const {
-		const float dist = UnsignedDistanceToPlane( point );
-		return ( dist < epsilon );
-	}
-
-	bool IsCoPlanarOrBehind( const Vec3d & point, const float epsilon = 0.00001f ) const {
-		const float dist = SignedDistanceToPlane( point );
-		return ( dist < epsilon );
-	}
-
-	Vec3d ProjectPointOntoPlane( const Vec3d & point ) const {
-		const float distance = SignedDistanceToPlane( point );
-		const Vec3d ptOnPlane = point - m_normal * distance;
-		return ptOnPlane;
-	}
-};
+#include "plane.h"
 
 /*
 ====================================================
