e5a8873e271742ed70fb23ddb6a934ddb0f0daeb
ghodges413 <38189889+ghodges413@users.noreply.github.com>
2019-05-06 20:35:46 -0500

    Added compiler and memory barrier definitions to the threads common header

--- Algorithms/code/threading/Common.h
+++ Algorithms/code/threading/Common.h
@@ -25,3 +25,22 @@
 	#include <mutex>
 	#include <atomic>
 #endif
+
+/*
+===============================
+Barriers
+===============================
+*/
+
+// _ReadWriteBarrier is a windows msvc compiler barrier
+// Compiler barriers prevent the compiler from re-ordering the output assembly instructions
+// MemoryBarrier adds an instruction to prevent the cpu from re-ordering the execution of instructions
+#define MEMORY_BARRIER() MemoryBarrier()
+
+#if defined( WINDOWS_THREADS )
+	//#pragma intrinsic( _ReadWriteBarrier )
+	#define COMPILER_BARRIER() _ReadWriteBarrier()
+#else
+	// This is the gcc version of a compiler barrier
+	#define COMPILER_BARRIER() asm volatile("" ::: "memory")
+#endif
